f72680c4396e482d76e8e9b60f9ee8fe6f53bdf2
arsize <arsizeof3416@gmail.com>
2021-07-03 15:16:20 +0800

    feat:template

--- bin/cga.js
+++ bin/cga.js
@@ -7,8 +7,15 @@ program
     .version('0.0.1', '-v,--version')
     .command('create <name>')
     .description('create a new project')
-    .action(name => {
+    .action((name,) => {
         create(name)
     })
+
+program.on('--help', function () {
+    console.log('  Examples:');
+    console.log('');
+    console.log('    this is an example');
+    console.log('');
+});
 
 program.parse()
--- command/create.js
+++ command/create.js
@@ -1,19 +1,67 @@
 const inquirer = require('inquirer')
 const userList = require("../prompt/inquirerList")
-const download = require('download-git-repo')
-
+const download = require('../lib/download')
+const ora = require('ora');
+const chalk = require('chalk')
+const reName = require("../lib/rename")
+const shell = require('../lib/shell')
+const spinner = ora('Installing CLI plugins. This might take a while2...')
 
 function create(name) {
     inquirer.prompt(userList).then(answer => {
-        console.log(answer)
-        console.log("⚙️   Installing CLI plugins. This might take a while2...")
-        download('arsize/cgaTemplate#vue2_default', 'teamp', { clone: true }, function (err) {
-            console.log(err ? 'Error' : 'Success')
-            console.log('err', err)
+        spinner.start()
+        let tempName = handleAnswer(answer)
+        download(tempName, "./", function (err) {
+            spinner.stop()
+            if (err) {
+                console.error(err)
+                return
+            }
+            // 后续处理
+            Promise.all(task(name)).then(res => {
+                console.log(chalk.green("Success"))
+                console.log(chalk.green("enjoy! :)"))
+            })
+
         })
 
     })
 
+}
+
+/**
+ * 处理用户选择结果，确定模板
+ * @param {object} answer 
+ * @returns 
+ */
+function handleAnswer(answer) {
+    let templateName = ""
+    if (answer.projectType == 'vue') {
+        templateName += answer.checkVersion
+        if (answer.modulesCustom == 'default') {
+            // vue2 + vue-cli3
+            templateName += `_${answer.modulesCustom}`
+        }
+
+    } else if (answer.projectType == 'react') {
+
+    }
+
+    return templateName
+}
+
+/**
+ * 任务流程
+ * @param {string} name 
+ * @returns 
+ */
+function task(name) {
+    let temp = [
+        reName('package.json', name),
+        reName('package-lock.json', name),
+        shell('npm i')
+    ]
+    return temp
 }
 
 module.exports = create
